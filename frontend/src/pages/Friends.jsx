6a1edafc4916003dff621e5262945084ef0a7e83
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2025-04-30 22:00:33 +0300

    Added frontend components to the friends and profile pages

--- frontend/src/pages/Friends.jsx
+++ frontend/src/pages/Friends.jsx
@@ -1,9 +1,20 @@
-
+import {useState} from 'react'
 
 function Friends() {
+    const [search, setSearch] = useState('');
+    async function lookForUser() {
+        const response = await fetch(`http://127.0.0.1:8000/api/users/${search}`);
+    }
     return (
         <>
-        <h1>Friends</h1>
+        <form onSumblit={(e) => {
+            e.preventDefault()
+            
+            }}>
+         <label for='search'>Look for a user</label>
+         <input id='search' type='text' value={search} onChange={(e) => setSearch(e.target.value)}/>
+         <button type='submit'>Search</button>
+         </form>
         </>
     )
 }
--- frontend/src/pages/ProfileSettings.jsx
+++ frontend/src/pages/ProfileSettings.jsx
@@ -1,9 +1,44 @@
+import { useState } from "react"
+import axios from "axios"
 
+function ProfileSettings({loggedUser}) {
+    const [userPosts, setUserPosts] = useState([]);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
-function ProfileSettings() {
+    async function createPost(title, content) {
+        const response = await axios.post('http://127.0.0.1:8000/api/posts/', {title, content, loggedUser});
+        try {
+            if(response.status === 201) setUserPosts([...userPosts, response.data]);
+            else throw new Error(response.data);
+        } catch(err) {
+            alert(err);
+        }
+    }
     return (
         <>
-        <h1>Profile Settings!</h1>
+            <h1>Your posts: </h1>
+                <div>
+                    {userPosts.map((post) => {
+                        return (
+                            <div>
+                                <h2>{post.title}</h2>
+                                <p>{post.content}</p>
+                            </div>
+                        )
+                    })}   
+                </div>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                createPost(title, content, loggedUser);
+            }}>
+                <h2>New Post</h2>
+                <label for="title">Title</label>
+                <input id="title" type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+                <label for="content">Content</label>
+                <input id="content" type="text" value={content} onChange={(e) => setContent(e.target.value)}/>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
