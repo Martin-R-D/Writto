70dcec3457bb3c5971d50f7655375a919602d625
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2025-05-05 22:06:16 +0300

    Registration fixed
    Now the registration view works properly, permission classes are set to AllowAny

--- backend/writtoBackend/api/views.py
+++ backend/writtoBackend/api/views.py
@@ -4,9 +4,11 @@ from rest_framework.response import Response
 from .serializers import RegistrationSerializer, PostsSerializer
 from rest_framework import viewsets
 from .models import Posts
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class RegistrationView(APIView):    
+    permission_classes = [AllowAny]
+    
     def post(self, request):    
         serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
